384d4247693e583f23e56bd03a42fec01dfc3eaf
Saikot hasan <49225310+saikothasan@users.noreply.github.com>
2024-12-23 19:49:06 +0600

    Update Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { CircularProgress, Box } from '@mui/material';
 
 const Loader: React.FC = () => {
   return (
-    <div className="flex items-center justify-center mt-6">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
-    </div>
+    <Box display="flex" justifyContent="center" alignItems="center" mt={4}>
+      <CircularProgress />
+    </Box>
   );
 };
 
